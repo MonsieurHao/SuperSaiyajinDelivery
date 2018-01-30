d0df5cdb3a5902dae4c3dce02e6bb1bdd7590688
Oualid <oualid.boushaba@gmail.com>
2018-01-30 08:27:11 +0100

    final push

--- src/Stock.java
+++ src/Stock.java
@@ -6,8 +6,17 @@ public class Stock {
 
 	static
 	 {
-		stock_hashmap.put("Figurine", 10);
-		stock_hashmap.put("DragonBall", 25);
+		stock_hashmap.put("Senzu Bean", 10);
+		stock_hashmap.put("DragonBall", 15);
+		stock_hashmap.put("Sangoku", 20);
+		stock_hashmap.put("Vegeta", 25);
+		stock_hashmap.put("Picolo", 30);
+		stock_hashmap.put("Gohan", 18);
+		stock_hashmap.put("Gogeta Ultra instinct", 14);
+		stock_hashmap.put("Kefla", 8);
+		stock_hashmap.put("Yamcha", 3);
+		stock_hashmap.put("C-18", 7);
+		stock_hashmap.put("Flying Nimbus", 21);
 	 }
 
 
